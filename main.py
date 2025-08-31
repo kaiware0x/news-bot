2fcd3219a06618b16e8b68389b66b664216a9359
Suzuki Takumi [NS] <suzuki.takumi@nintendo-systems.com>
2025-09-01 00:23:45 +0900

    feat: 複数記事の送信に対応

--- main.py
+++ main.py
@@ -9,48 +9,34 @@ NEWS_COUNT = int(os.getenv("NEWS_COUNT", 5))
 RSS_FEEDS = os.getenv("RSS_FEEDS", "https://rss.itmedia.co.jp/rss/2.0/news_bursts.xml").split(",")
 
 def fetch_news():
-    all_articles = []
+    all_articles = {}
     for feed_url in RSS_FEEDS:
-        one_site_articles = []
         feed = feedparser.parse(feed_url.strip())
-        # for entry in feed.entries:
-        #     pub_date = None
-        #     if hasattr(entry, "published"):
-        #         try:
-        #             pub_date = date_parser.parse(entry.published)
-        #         except Exception:
-        #             pub_date = None
-        #     one_site_articles.append({
-        #         "title": entry.title,
-        #         "link": entry.link,
-        #         "published": pub_date
-        #     })
-        all_articles += feed.entries[:NEWS_COUNT]
-    # 日付があるものは新しい順に、なければ後回し
-    # all_articles.sort(key=lambda x: x["published"] or datetime.min, reverse=True)
+        all_articles[feed_url] = feed.entries[:NEWS_COUNT]
     return all_articles
 
-def send_to_discord(articles):
-    embeds = []
-    for a in articles:
-        embed = {
-            "title": a.title,          # 記事タイトル
-            "url": a.link,             # クリックで飛べるリンク
-            "description": (a.summary if hasattr(a, "summary") else "（要約なし）"),
-            "color": 0x1E90FF          # 青色（16進数カラーコード）
+def send_to_discord(all_articles: dict):
+    for feed_url, articles in all_articles.items():
+        embeds  = []
+        for a in articles:
+            embed = {
+                "title": a.title,          # 記事タイトル
+                "url": a.link,             # クリックで飛べるリンク
+                "description": (a.summary if hasattr(a, "summary") else "（要約なし）"),
+                "color": 0x1E90FF          # 青色（16進数カラーコード）
+            }
+            embeds.append(embed)
+
+        data = {
+            "content": f"📰 今日のITニュース on {feed_url}",
+            "embeds": embeds
         }
-        embeds.append(embed)
 
-    data = {
-        "content": "📰 今日のITニュースはこちら！",
-        "embeds": embeds
-    }
-
-    response = requests.post(DISCORD_WEBHOOK_URL, json=data)
-    if response.status_code == 204:
-        print("✅ Discordに送信しました！（Embed形式）")
-    else:
-        print(f"⚠️ Discord送信失敗: {response.status_code}, {response.text}")
+        response = requests.post(DISCORD_WEBHOOK_URL, json=data)
+        if response.status_code == 204:
+            print(f"✅ Discordに送信しました: {feed_url}")
+        else:
+            print(f"⚠️ Discord送信失敗: {response.status_code}, {response.text}")
 
 if __name__ == "__main__":
     news = fetch_news()
